088922e18570b9d4d74d278b5ccea18b13c16691
Concourse <concourse@finbourne.com>
2024-04-12 10:19:17 +0000

    Updates for v2.0.30

--- sdk/src/main/java/com/finbourne/scheduler/Configuration.java
+++ sdk/src/main/java/com/finbourne/scheduler/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.scheduler;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.29";
+    public static final String VERSION = "2.0.30";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
