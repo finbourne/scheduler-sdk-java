929ef7b25968bf0857c1d30d06c850445553250a
Concourse <concourse@finbourne.com>
2025-04-29 15:12:50 +0000

    scheduler java updates for v2.0.129
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/scheduler/Configuration.java
+++ sdk/src/main/java/com/finbourne/scheduler/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.scheduler;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.128";
+    public static final String VERSION = "2.0.129";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
