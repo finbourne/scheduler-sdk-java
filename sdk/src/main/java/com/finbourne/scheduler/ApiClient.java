3e3059c6bbaf65ee063a54aa87719d32283af762
Concourse <concourse@finbourne.com>
2025-07-14 14:07:39 +0000

    scheduler java updates for v2.0.146
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/scheduler/ApiClient.java
+++ sdk/src/main/java/com/finbourne/scheduler/ApiClient.java
@@ -212,7 +212,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.145/java");
+        setUserAgent("OpenAPI-Generator/2.0.146/java");
 
         authentications = new HashMap<String, Authentication>();
     }
